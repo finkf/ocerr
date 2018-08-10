64d4005e072183a1f1869a21a713eeed3f0ae92b
flo <flo@cis.lmu.de>
2018-08-10 14:19:44 +0200

    Finish stat subcommand

--- stat.go
+++ stat.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/finkf/lev"
 	"github.com/spf13/cobra"
@@ -16,16 +17,21 @@ var (
 		RunE:  stat,
 		Args:  cobra.ExactArgs(0),
 	}
-	statGlobal = make(map[byte]uint)
-	statLocal  = make(map[byte]uint)
-	statMax    = 0
+	statGlobal        = make(map[byte]uint)
+	statLocal         = make(map[byte]uint)
+	statErrorPatterns = make(map[spair]uint)
+	statMax           = 0
 )
+
+type spair struct {
+	first, second string
+}
 
 // Args: --max (max number of stats 0=all)
 //       --error-stats print error stats
 func init() {
 	statCmd.Flags().IntVarP(&statMax, "max", "m",
-		0, "set maximal number of printed error patterns")
+		0, "set maximal number of printed error patterns (0=all, -1=none)")
 }
 
 func stat(cmd *cobra.Command, args []string) error {
@@ -35,8 +41,10 @@ func stat(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	_, err = fmt.Printf("global: %s\n", countsToString(statGlobal))
-	return err
+	if _, err := fmt.Printf("Global: %s\n", countsToString(statGlobal)); err != nil {
+		return err
+	}
+	return printErrorPatterns()
 }
 
 func statBlock(b block) error {
@@ -49,8 +57,65 @@ func statBlock(b block) error {
 		statLocal[op]++
 		statGlobal[op]++
 	}
+	addErrorPatterns(b)
 	b.stats = countsToString((statLocal))
 	return printBlock(b, os.Stdout)
+}
+
+func addErrorPatterns(b block) {
+	if statMax < 0 {
+		return
+	}
+	for i := 0; i < len(b.a.Trace); {
+		op := b.a.Trace[i]
+		if op == lev.Nop {
+			i++
+			continue
+		}
+		j := i + 1
+		for ; j < len(b.a.Trace); j++ {
+			if b.a.Trace[j] != op {
+				break
+			}
+		}
+		p := spair{string(b.a.S1[i:j]), string(b.a.S2[i:j])}
+		statErrorPatterns[p]++
+		i = j
+	}
+}
+
+func printErrorPatterns() error {
+	if statMax < 0 {
+		return nil
+	}
+	var ep []struct {
+		p spair
+		c uint
+	}
+	var total uint
+	for p, c := range statErrorPatterns {
+		total += c
+		ep = append(ep, struct {
+			p spair
+			c uint
+		}{p, c})
+	}
+	sort.Slice(ep, func(i, j int) bool {
+		return ep[i].c > ep[j].c
+	})
+	if _, err := fmt.Printf("Errors: %d\n", total); err != nil {
+		return err
+	}
+	for i := range ep {
+		if statMax > 0 && i >= statMax {
+			break
+		}
+		if _, err := fmt.Printf("%d:\t{%s}\t{%s}\n",
+			ep[i].c, ep[i].p.first, ep[i].p.second); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func countsToString(counts map[byte]uint) string {
